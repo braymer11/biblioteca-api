1caad96c52af70543e82744c07071452bb9ed90d
braymer11 <142228537+braymer11@users.noreply.github.com>
2024-12-15 15:25:08 -0400

    libro.js

--- models/models/libro.js
+++ models/models/libro.js
@@ -1,8 +1,9 @@
-class Autor {
-    constructor(id, nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
-}
+const Libro = require('../models/libro');
 
-module.exports = Autor;
+const libros = [
+    new Libro(1, "Cien años de soledad", 1, true),
+    new Libro(2, "Harry Potter y la piedra filosofal", 2, false),
+    new Libro(3, "El señor de los anillos", 3, true),
+];
+
+module.exports = libros;
