8b1c3aa96fe578610eb94053ad339002ce941d1a
braymer11 <142228537+braymer11@users.noreply.github.com>
2024-12-15 15:25:36 -0400

    autor.js

--- models/autor.js
+++ models/autor.js
@@ -1,11 +1,11 @@
-// models/autor.js
-class Autor {
-    constructor(id, nombre, fechaNacimiento) {
-      this.id = id;
-      this.nombre = nombre;
-      this.fechaNacimiento = fechaNacimiento;
-    }
-  }
-  
-  module.exports = Autor;
+const Autor = require('../models/autor');
+
+const autores = [
+    new Autor(1, "Gabriel García Márquez"),
+    new Autor(2, "J.K. Rowling"),
+    new Autor(3, "J.R.R. Tolkien"),
+];
+
+module.exports = autores;
+
   
